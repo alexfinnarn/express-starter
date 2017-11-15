26a19b0013fe0485398190e690d7b08d6db8afac
Alex Finnarn <alexfinnarn@gmail.com>
2017-11-15 10:14:37 -0700

    add user prompts

--- scripts/build-express.sh
+++ scripts/build-express.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
+# Define Drupal version.
+echo What version of Drupal would you like to download?
+read DRUPAL
+
 # Download Drupal.
 echo Downloading Drupal...
-drush dl drupal-7.56
+drush dl drupal-${DRUPAL}
 echo Drupal downloaded.
 
 # Move Drupal to docroot.
 echo Copying Drupal to docroot.
-mv drupal-7.56/.[!.]* web
-mv drupal-7.56/* web
-rm -rf drupal-7.56
+mv drupal-${DRUPAL} web
 echo Drupal copied to docroot.
 
-# Clone Express.
+# Define Express branch.
+echo What branch of Express would you like to checkout?
+read EXPRESS
+
+# Checkout Express.
 echo Cloning and checking out Express.
 cd web/profiles
-git clone -b dev git@github.com:CuBoulder/express.git
-
-# Checkout latest tag.
+git clone git@github.com:CuBoulder/express.git
 cd express
-latesttag=$(git describe --tags)
-echo checking out ${latesttag}
-git checkout ${latesttag}
+
+#latesttag=$(git describe --tags)
+echo checking out ${EXPRESS}
+git checkout ${EXPRESS}
 
