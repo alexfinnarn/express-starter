f34c4e9562b6d9e3bbb4f48f47c3387ff228ebd2
Alex Finnarn <alexfinnarn@gmail.com>
2017-12-13 12:55:07 -0700

    adding DSLM support

--- scripts/build-express.sh
+++ scripts/build-express.sh
@@ -1,30 +1,47 @@
 #!/usr/bin/env bash
 
-# Define Drupal version.
-echo What version of Drupal would you like to download?
-read DRUPAL
+DRUPAL_TAG=7.56-hardered
+EXPRESS_TAG=2.8.0
+
+# Store local project root.
+ROOT=$(pwd)
+echo Project root is: ${ROOT}
+
+# Export DSLM variables.
+drush dl dslm-7.x
+export DSLM_BASE="/app/code/dslm_base"
+export PACKAGES_BASE="/app/code/packages_base"
 
 # Download Drupal.
 echo Downloading Drupal...
-drush dl drupal-${DRUPAL}
-echo Drupal downloaded.
-
-# Move Drupal to docroot.
-echo Copying Drupal to docroot.
-mv drupal-${DRUPAL} web
-echo Drupal copied to docroot.
-
-# Define Express branch.
-echo What branch of Express would you like to checkout?
-read EXPRESS
+cd ${ROOT}/code/dslm_base/cores
+git clone git@github.com:CuBoulder/drupal-7.x.git ${DRUPAL_TAG}
+cd ${DRUPAL_TAG}
+git checkout ${DRUPAL_TAG}
 
 # Checkout Express.
 echo Cloning and checking out Express.
-cd web/profiles
-git clone git@github.com:CuBoulder/express.git
-cd express
+cd ${ROOT}/code/dslm_base/profiles
+git clone git@github.com:CuBoulder/express.git express-${EXPRESS_TAG}
+cd express-${EXPRESS_TAG}
 
 #latesttag=$(git describe --tags)
-echo checking out ${EXPRESS}
-git checkout ${EXPRESS}
+echo checking out ${EXPRESS_TAG}
+git checkout ${EXPRESS_TAG}
 
+# Move Drupal to docroot.
+cd ${ROOT}/web
+drush dslm new
+
+# Install site
+#echo Installing site...
+#drush si express --db-url=mysql://drupal7:drupal7@database:3306/drupal7 -y
+
+# Install Behat dependencies.
+#echo Installing Behat dependencies...
+#cd profiles/express/test/behat
+#composer install
+
+# Start up headless Chrome.
+#echo Starting headless Chrome...
+#google-chrome-stable --disable-gpu --headless --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 > /dev/null 2>&1 &
