14cc031560b31e67a88c26c53756b1cc82be8c56
Alex Finnarn <alexfinnarn@gmail.com>
2017-12-19 10:06:10 -0700

    modifyinh mysql and php config

--- scripts/add-tools.sh
+++ scripts/add-tools.sh
@@ -2,3 +2,7 @@
 
 # Download DSLM since drush wasn't available on "lando start".
 drush dl dslm-7.x
+
+# Echo in MySQL settings since the config file load doesn't stick.
+mysql -u root -h database -e "set @@global.innodb_file_per_table=0;"
+mysql -u root -h database -e "set @@global.innodb_flush_log_at_trx_commit=2;"
