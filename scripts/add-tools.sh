5b1ed6b0ccbaad4c3dd6a3e50cbebc50827b292b
Alex Finnarn <alexfinnarn@gmail.com>
2018-02-27 15:29:45 -0700

    commiting gitignore

--- scripts/add-tools.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Download DSLM since drush wasn't available on "lando start".
-drush dl dslm-7.x
-
-# Echo in MySQL settings since the config file load doesn't stick.
-mysql -u root -h database -e "set @@global.innodb_file_per_table=0;"
-mysql -u root -h database -e "set @@global.innodb_flush_log_at_trx_commit=2;"
